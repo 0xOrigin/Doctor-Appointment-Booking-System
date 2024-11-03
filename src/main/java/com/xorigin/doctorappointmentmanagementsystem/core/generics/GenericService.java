55e6ab7d20b66471b302455f2bfe863a7b2e58ec
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-11-03 20:56:00 +0200

    Add Auditing functionality

--- src/main/java/com/xorigin/doctorappointmentmanagementsystem/core/generics/GenericService.java
+++ src/main/java/com/xorigin/doctorappointmentmanagementsystem/core/generics/GenericService.java
@@ -7,18 +7,18 @@ import java.util.Optional;
 
 public abstract class GenericService<T, R extends GenericRepository<T, ?>, DTO> {
 
-    private final AuditorAware<User> currentAuditor;
+    private final AuditorAware<User> auditorAware;
     private final R repository;
     private final DTO dto;
 
-    public GenericService(AuditorAware<User> currentAuditor, R repository, DTO dto) {
-        this.currentAuditor = currentAuditor;
+    public GenericService(AuditorAware<User> auditorAware, R repository, DTO dto) {
+        this.auditorAware = auditorAware;
         this.repository = repository;
         this.dto = dto;
     }
 
     public Optional<User> getCurrentUser() {
-        return currentAuditor.getCurrentAuditor();
+        return auditorAware.getCurrentAuditor();
     }
 
     public R getRepository() {
